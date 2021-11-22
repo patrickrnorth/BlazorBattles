2f84644e6b075cf2f166cc3140b594c2037fe310
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2021-11-22 15:20:44 -0500

    Create new index, TopMenu, Main layout pages and created logic for banana resource

---
+++ BlazorBattles/Client/Services/IBananaService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface IBananaService 
+    {
+        event Action OnChange;
+        
+        int Bananas { get; set; }
+        void EatBananas(int amount);
+
+        void AddBananas(int amount);
+    }
+}
