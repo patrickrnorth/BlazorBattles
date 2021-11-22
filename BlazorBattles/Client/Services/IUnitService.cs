b91b514160887fbb07a3f9354375d31475c9a051
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2021-11-22 15:41:12 -0500

    Add Units with the Unit Service

---
+++ BlazorBattles/Client/Services/IUnitService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorBattles.Shared;
+using System.Collections.Generic;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface IUnitService
+    {
+        IList<Unit> Units { get; }
+        IList<UserUnit> MyUnits { get; set; }
+        void AddUnit(int unitId);
+
+    }
+}
